294130f4179ec90b432dc8da656497a40bac2600
Raymond Chow <raychow1944@yahoo.com>
2018-12-20 01:31:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(arr)
   line_str = "The line is currently: "
   if arr.empty? == true
-    puts "The line is currently empty."
+    line_str = "The line is currently empty."
   else 
     arr.each_with_index do |name, i|
       line_str += "#{i}. #{name}"
