cfd1e6891b745551409c22def8fe48ff1ac7237b
Raymond Chow <raychow1944@yahoo.com>
2018-12-20 01:32:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(arr)
     line_str = "The line is currently empty."
   else 
     arr.each_with_index do |name, i|
-      line_str += " #{i+1}. #{name}"
+      line_str += "#{i+1}. #{name} "
     end
   end
   puts line_str
