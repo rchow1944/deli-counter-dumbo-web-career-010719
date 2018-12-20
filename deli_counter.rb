de0c6020d0f51dade63153b785a93a3e4ff7ee4c
Raymond Chow <raychow1944@yahoo.com>
2018-12-20 01:43:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,26 @@
 # Write your code here.
 def line(arr)
-  if arr.empty?
-    "The line is currently empty."
+  line_str = "The line is currently: "
+  if arr.empty? == true
+    line_str = "The line is currently empty."
   else 
-    arr.count
+    arr.each_with_index do |name, i|
+      line_str += "#{i+1}. #{name} "
+    end
+    line_str.chop!
+  end
+  puts line_str
+end
+
+def take_a_number(arr, str)
+  arr << str
+  puts "Welcome, #{str}. You are number #{arr.count} in line."
+end
+
+def now_serving(arr)
+  if arr.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{arr.shift}."
   end
 end
